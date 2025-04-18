6a84846ae6a50c2a0114d48a54ebe5cb0ede1311
fotapol <krupinkd@gmail.com>
2025-04-18 11:34:57 +0200

    refactor: Code styling one file with ruff formater

--- app/models/profile.py
+++ app/models/profile.py
@@ -15,9 +15,7 @@ class Profile(ProfileBase, table=True):
     id: uuid.UUID | None = Field(default_factory=uuid.uuid4, primary_key=True)
     user_id: uuid.UUID = Field(index=True, unique=True, nullable=False)
     created_at: datetime.datetime = Field(
-        sa_column=Column(
-            DateTime(timezone=True), default=func.now(), nullable=False
-        )
+        sa_column=Column(DateTime(timezone=True), default=func.now(), nullable=False)
     )
     updated_at: datetime.datetime = Field(
         sa_column=Column(
